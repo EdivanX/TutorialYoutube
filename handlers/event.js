f298f25f55b3165eee6eb9d9a7f18fc3a0db0a17
YaekDev <67657401+YaekDev@users.noreply.github.com>
2020-11-10 18:59:50 -0300

    Create event.js

---
+++ handlers/event.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+module.exports = (client) => {
+  const c = local => {
+    const events = fs.readdirSync(`./events/${local}/`).filter(x => x.endsWith('.js'));
+    for(let file of events) {
+      const l = require(`../events/${local}/${file}`)
+      let nome = file.split('.')[0];
+      client.on(nome, l.bind(null, client));
+    }
+  }
+
+  ["client"].forEach(x => c(x))
+}
